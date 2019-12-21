f34805a15b5426ef03028d1f59504f7f893089cc
sairam143 <sairamjagu143@gmail.com>
2019-12-21 23:08:41 +0530

    initialcommit

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+	<title>
+		:: ONLINE GIFTING SYSTEM ::
+	</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha256-KsRuvuRtUVvobe66OFtOQfjP8WA2SzYsmm4VPfMnxms=" crossorigin="anonymous"></script>
+ 
+</head>
+<style type="text/css">
+	.bg-overlay {
+    background: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)),url("photos/w1.jpg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    background-position: center center;
+    height: 100%;
+   
+}
+.txt
+{
+	color: #fff;
+    height: 450px;
+    
+}
+h1
+{
+	background: linear-gradient(to right, #30CFD0 0%, #330867 100%);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	font-family: ;
+	font-size: 60px;
+}
+body
+{
+	margin: 0px;
+	 overflow-x: hidden;
+	  overflow-y: hidden;
+}
+</style>
+
+<body >
+<div class="bg-overlay" data-aos="zoom-in" data-aos-duration="2000">
+	<div class="row text-center txt" data-aos="zoom-in-up" data-aos-duration="2000">
+		<h1>WELCOME</h1>
+        <h4>You can just use the "<strong>.bg-overlay</strong>" class on any container/element,<br>
+        and specify the image you want to use and its height.</h4>
+        <br><br>
+        <button type="button" class="btn btn-primary btn-lg">Get Started</button>
+	</div>
+</div>
+
+</body>
+ <script>
+  AOS.init();
+</script>
+</html>
